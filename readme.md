fbb881fcbf43ff10143923d8cdd01487da21dfcc
kirilcom <kirill_seo@tutanota.com>
2019-12-27 22:14:54 +0300

    NEW

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # ТЕСТОВЫЙ РЕП
+commands:
+git config --global user.email "kirill_seo@tutanota.com"
+  git config --global user.name "kirilcom"
+  
+  kirill_seo@tutanota.com
+kirilcom  pass: ijVFS5$#&gJtQ
---
+++ script.py
@@ -0,0 +1,9 @@
+# если хотим получить отфильтрованный лист, то будет даже так
+filteredSequence = []
+
+for num in sequence:
+    if num % 5 == 0:
+        filteredSequence.append( num )
+
+print( filteredSequence )
+#dvdfv dfds sdfdsf
